e1b347497e615dba676bd7d83b6d12052eac1134
Wunmicrown <olayiwolapamilerin34@gmail.com>
2024-07-02 00:39:43 +0100

    made an adjusment on index.html file.

--- my-tech-goal/script.js
+++ my-tech-goal/script.js
@@ -1,5 +1,12 @@
 document.addEventListener("DOMcontnentLoaded", () =>{
   const updateTime = () =>{
-    
+    const now = new Date()
+    const timeString=now.toUTCString().split('  ')
+
+    document.getElementById('currentTime').textContent = timeString;
+    document.getElementById('currentDay').textContent = dayString;
   }
+  updateTime();
+  setInterval(updateTime, 1000);
+
 })
