46b4dc76c68b9f65560eada6158040e0b8fcc615
Wunmicrown <olayiwolapamilerin34@gmail.com>
2024-07-02 01:12:22 +0100

    Changes were made to script file. fixed the issued of wrong name func.

--- my-tech-goal/script.js
+++ my-tech-goal/script.js
@@ -1,11 +1,13 @@
-document.addEventListener("DOMcontnentLoaded", () =>{
-  const updateTime = () =>{
+document.addEventListener("DOMContentLoaded", () => {
+  const updateTime = () => {
     const now = new Date()
-    const timeString=now.toUTCString().split('  ')
+    const timeString = now.toUTCString().split(' ')[4];
+    const dayString = now.toLocaleDateString('en-US', { weekday: 'long' });
 
     document.getElementById('currentTime').textContent = timeString;
     document.getElementById('currentDay').textContent = dayString;
-  }
+  };
+  
   updateTime();
   setInterval(updateTime, 1000);
 
